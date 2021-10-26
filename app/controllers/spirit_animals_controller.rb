6dbc77f83dd2588d82b4b1c0cde4b8da01215314
Luke Billings <lukecbillings@gmail.com>
2021-10-26 19:26:15 +0100

    created methods for spirit andimal controller

--- app/controllers/spirit_animals_controller.rb
+++ app/controllers/spirit_animals_controller.rb
@@ -1,2 +1,19 @@
 class SpiritAnimalsController < ApplicationController
+
+  def index
+    @spirit_animals = SpiritAnimal.all
+  end
+
+  def new
+    @spirit_animal = SpiritAnimal.new
+  end
+
+  def create
+    @spirit_animal.user = SpiritAnimal.new(spirit_animal_params)
+    @spirit_animal.user = current_user
+    if @spirit_animal.save
+      redirect_to index_path
+    end
+  end
+
 end
