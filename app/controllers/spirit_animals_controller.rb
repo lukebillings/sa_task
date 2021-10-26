1196c233c5bd937daaed608ba8981a00a655fa90
Luke Billings <lukecbillings@gmail.com>
2021-10-26 20:28:00 +0100

    added params to private

--- app/controllers/spirit_animals_controller.rb
+++ app/controllers/spirit_animals_controller.rb
@@ -1,5 +1,8 @@
 class SpiritAnimalsController < ApplicationController
 
+# can test in postman
+  # skip_before_action :verify_authenticity_token
+
   def index
     @spirit_animals = SpiritAnimal.all
   end
@@ -9,11 +12,17 @@ class SpiritAnimalsController < ApplicationController
   end
 
   def create
-    @spirit_animal.user = SpiritAnimal.new(spirit_animal_params)
+    @spirit_animal = SpiritAnimal.new(spirit_animal_params)
     @spirit_animal.user = current_user
     if @spirit_animal.save
       redirect_to index_path
     end
   end
+
+  private
+
+    def spirit_animal_params
+      params.require(:spirit_animal).permit(:name)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'spirit_animals#index'
-  post '/', to: 'spirit_animals#new'
-  get '/', to: 'spirit_animals#create'
+  get '/', to: 'spirit_animals#new'
+  post '/', to: 'spirit_animals#create'
 end
