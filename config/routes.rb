a6ef9a4671307d5770bf1c75551b3b2d15536afc
Luke Billings <lukecbillings@gmail.com>
2021-10-26 19:29:04 +0100

    added routes for spirit animal

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'spirit_animals#index'
+  get '/', to: 'spirit_animals#new'
+  get '/', to: 'spirit_animals#create'
 end
