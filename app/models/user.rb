f2635e2974827d2f37c1130f2b8ed6e3581980be
Luke Billings <lukecbillings@gmail.com>
2021-10-27 23:09:30 +0100

    added default false when signing up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :spirit_animals
+  has_many :spirit_animals
+
+
+  # attr_accessor :email, :password, :admin
+
 end
